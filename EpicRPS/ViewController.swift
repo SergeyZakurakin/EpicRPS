24b78174c872c13089f33a1e65e0cf91a94b19eb
Evgenii Mazrukho <mazrukho@gmail.com>
2024-06-10 02:19:57 +0300

    add files

---
+++ EpicRPS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EpicRPS
+//
+//  Created by Evgenii Mazrukho on 10.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
