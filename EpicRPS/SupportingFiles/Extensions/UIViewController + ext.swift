63631f0c4282874ebec54844f0cf37dde3af8e02
Evgenii Mazrukho <mazrukho@gmail.com>
2024-06-13 01:25:55 +0300

    add navigation, add assets

---
+++ EpicRPS/SupportingFiles/Extensions/UIViewController + ext.swift
@@ -0,0 +1,28 @@
+//
+//  UIViewController + ext.swift
+//  EpicRPS
+//
+//  Created by Evgenii Mazrukho on 12.06.2024.
+//
+
+import UIKit
+
+extension UIViewController {
+    
+    func setupNavBar(on vc: UIViewController,
+                     title: String?,
+                     leftImage: UIImage, leftSelector: Selector?,
+                     rightImage: UIImage, rightSelector: Selector?) {
+        
+        let leftBarButton = UIBarButtonItem(image: leftImage.withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: leftSelector)
+        
+        let rightBarButton = UIBarButtonItem(image: rightImage.withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: rightSelector)
+        
+        navigationController?.navigationBar.tintColor = .greyBlack
+        
+        vc.title = title
+        vc.navigationItem.setHidesBackButton(true, animated: true)
+        vc.navigationItem.leftBarButtonItem = leftBarButton
+        vc.navigationItem.rightBarButtonItem = rightBarButton
+    }
+}
