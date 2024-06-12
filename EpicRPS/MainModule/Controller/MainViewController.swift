61d4b62b9d7051c17d2698194d76d1508a87bb67
Evgenii Mazrukho <mazrukho@gmail.com>
2024-06-13 02:15:48 +0300

    Revert "Merge branch 'develop' of https://github.com/Bruzya/EpicRPS into develop"
    This reverts commit 941048cebeb367f0316eb0fc92740aabedc8bed9, reversing
    changes made to 1c40d6d28eb29ecc72054321c517c497e3951d2c.
    
    # Conflicts:
    #	EpicRPS.xcodeproj/project.pbxproj
    #	EpicRPS/MainModule/Controller/MainViewController.swift

--- EpicRPS/MainModule/Controller/MainViewController.swift
+++ EpicRPS/MainModule/Controller/MainViewController.swift
@@ -8,6 +8,55 @@
 import UIKit
 
 final class MainViewController: UIViewController {
+    
+    
+    // MARK: - UI
+    private lazy var buttonStackView: UIStackView = {
+        let element = UIStackView()
+        element.distribution = .fillEqually
+        element.axis = .vertical
+        element.alignment = .center
+        element.spacing = 10
+        
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var maleHandImageView: UIImageView = {
+        let element = UIImageView()
+        element.image = .mainMaleHand
+        element.contentMode = .scaleAspectFit
+        
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var femaleHandImageView: UIImageView = {
+        let element = UIImageView()
+        element.image = .mainFemaleHand
+        
+        element.translatesAutoresizingMaskIntoConstraints = false
+        return element
+    }()
+    
+    private lazy var mainTitleLabel = UILabel(textColor: .brownLight)
+    private lazy var shadowTitleLabel = UILabel(textColor: .brownDarker)
+    
+    private lazy var startButton = UIButton(textLabel: "START")
+    private lazy var resultButton = UIButton(textLabel: "RESULT")
+    
+    
+    // MARK: - Private methods
+    @objc private func startButtonPressed(_ sender: UIButton) {
+        print("START")
+    }
+    
+    @objc private func resultButtonPressed(_ sender: UIButton) {
+        print("RESULT")
+    }
+    
+    
+    // MARK: - Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
